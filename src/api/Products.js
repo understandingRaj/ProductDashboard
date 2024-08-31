180aa4098dc0e6f0bc10c822bd24d6c827f27087
Rajesh Kumar <146615871+understandingRaj@users.noreply.github.com>
2024-08-31 14:27:09 +0530

    Update Products.js

--- src/api/Products.js
+++ src/api/Products.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const fetchProducts = async () => {
   try {
     // const response = await axios.get('/Api.json'); // Fetches the JSON from public folder
-    const response = await axios.get('/interview-materials/products.json');
+    const response = await axios.get('https://cdn.drcode.ai/interview-materials/products.json');
     const products = response.data.products;
     // console.log(products);
     return Object.values(products); // Convert object to array
